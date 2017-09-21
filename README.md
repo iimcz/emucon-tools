3864f4e4bc4b7dae39c08007967c5452cab9b1c3
Oleg Stobbe <oleg.stobbe@rz.uni-freiburg.de>
2017-09-21 20:12:40 +0200

    Add installation scripts

--- README.md
+++ README.md
@@ -11,3 +11,20 @@ To bootstrap a system for a single terminal session, simply run in your shell:
 $ . bootstrap.sh
 ```
 
+
+# Installation
+
+To install `emucon-tools` and all dependencies into `/usr/local`, simply run:
+```
+$ ./install.sh --destination /usr/local
+```
+
+Some dependencies will be built from source and some installed using host's package manager.
+
+The previous command will also add a sudoers-configuration for the current user, to be able
+to run required privileged commands without asking user password. A different user can be
+specified with `--user <name>` option. For all available options, please run:
+```
+$ ./install.sh --help
+```
+
---
+++ install.sh
@@ -0,0 +1,139 @@
+#!/bin/sh
+
+__cmd_name=$(basename "$0")
+
+# Default install directory
+readonly default_dstdir='/usr/local'
+
+# Default sudoers directory
+readonly default_sudodir='/etc/sudoers.d'
+
+
+# ========== Helper Functions ==========
+
+__print_usage()
+{
+	cat <<- EOT
+	USAGE:
+	    ${__cmd_name} [-u <name>] [--sudoers-dir <path>] [--destination <path>]
+
+	DESCRIPTION:
+	    Installs emucon-tools and dependencies on the host system.
+
+	OPTIONS:
+	    -d, --destination <path>
+	        The path to install into. (default: ${default_dstdir})
+
+	    -u, --user <name>
+	        The name of the user for sudoers configuration.
+
+	    --sudoers-dir <path>
+	        The path for sudoers drop-in files. (default: ${default_sudodir})
+
+	EOT
+}
+
+
+# ========== Script's Begin ==========
+
+if ! which emucon-install > /dev/null ; then
+	echo 'Required emucon-tools are not in PATH!'
+	echo 'Please source bootstrap.sh first.'
+	echo 'Aborting...'
+	exit 1
+fi
+
+. emucon-init.sh
+
+# Parse script's command line arguments
+longopts='destination:,user:,sudoers-dir:,help'
+cmdargs=$(emucon_parse_cmdargs -s 'd:u:h' -l "${longopts}" -- "$@")
+if emucon_cmd_failed ; then
+	emucon_abort
+fi
+
+# Lookup parsed parameters and their arguments
+eval set -- ${cmdargs}
+while true ; do
+	case "$1" in
+		-d|--destination)
+			dstdir="$2"
+			shift 2 ;;
+		-u|--user)
+			user="$2"
+			shift 2 ;;
+		--sudoers-dir)
+			sudodir="$2"
+			shift 2 ;;
+		-h|--help)
+			__print_usage
+			emucon_exit ;;
+		--)
+			shift 1
+			break ;;
+		*)
+			emucon_print_invalid_cmdargs_error "${cmdargs}"
+			emucon_abort -v ;;
+	esac
+done
+
+# Safety check!
+if [ $# -ne 0 ] ; then
+	emucon_print_invalid_cmdargs_error "${cmdargs}"
+	emucon_abort -v
+fi
+
+# Runtime directory
+curdir=$(emucon_get_current_dir "$0")
+srcdir="${curdir}/runtime"
+
+# Install directory
+dstdir="${dstdir:-${default_dstdir}}"
+emucon_ensure_dir_exists "${dstdir}"
+
+# Sudoers directory
+sudodir="${sudodir:-${default_sudodir}}"
+emucon_ensure_dir_exists "${sudodir}"
+
+# Sudoers user
+user="${user:-$(id --user --name)}"
+
+emucon_print_info 'Creating temporary directory...'
+readonly tmpdir=$(mktemp -d '/tmp/emucon-XX')
+
+__cleanup()
+{
+	emucon_print_info 'Cleaning up...'
+	sudo rm -r -v "${tmpdir}"
+}
+
+# Setup an exit-trap
+trap __cleanup EXIT QUIT INT TERM
+
+emucon_print_info 'Building OCI tools...'
+cd "${curdir}/builder"
+./emucon-build tool --output-dir "${tmpdir}" || emucon_abort
+
+emucon_print_info 'Installing OCI tools...'
+emucon-install "${tmpdir}" "${dstdir}" || emucon_abort
+
+emucon_print_info 'Installing emucon-tools...'
+emucon-install "${srcdir}" "${dstdir}" || emucon_abort
+
+emucon_print_info 'Installing dependencies...'
+sudo apt-get update
+sudo apt-get install jq
+
+emucon_print_info "Installing sudoers configuration for user '${user}'..."
+sudosrc="$(emucon-paths sudoers.template)"
+sudotmp="${tmpdir}/emucon-pwdless-commands"
+sedcmds="s|{{install-dir}}|${dstdir}|g; s|{{user}}|${user}|g"
+cat "${sudosrc}" | sed "${sedcmds}" > "${sudotmp}" || emucon_abort
+if [ ! -w "${sudodir}" ] ; then
+	# Install directory not writable,
+	# use sudo for file operations
+	cmdprefix='sudo'
+fi
+
+${cmdprefix} install -v -m 440 "${sudotmp}" "${sudodir}" || emucon_abort
+
