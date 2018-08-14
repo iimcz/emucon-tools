671215c7b213ccca194b17b9c909fc8214662e08
Oleg Stobbe <oleg.stobbe@rz.uni-freiburg.de>
2018-08-14 15:46:27 +0200

    Add support for building/using multiple rootfs types

--- builder/commands/layer/layers/base/run.sh
+++ builder/commands/layer/layers/base/run.sh
@@ -9,14 +9,25 @@ __print_usage()
 {
 	cat <<- EOT
 	USAGE:
-	    ${__cmd_name} -o <path>
+	    ${__cmd_name} -t <type> [-s <size>] -o <path>
 
 	DESCRIPTION:
 	    Builds container's base layer.
 
 	OPTIONS:
-	    -o, --output-dir <path>
-	        Output directory for built files
+	    -t, --output-type <type>
+	        Output type for built layer:
+	            tree: Filesystem tree
+	            qcow: QCow container image
+
+	    -o, --output-path <path>
+	        Output path for built layer
+
+	    -s, --output-size <size>
+	        Output image size in bytes (with optional suffix K, M or G)
+
+	    -n, --use-nbd <dev>
+	        Connect qcow-image to specified NBD device
 
 	EOT
 }
@@ -32,8 +43,8 @@ if [ $# -eq 0 ] ; then
 fi
 
 # Parse script's command line arguments
-shortopts='o:h'
-longopts='output-dir:,help'
+shortopts='t:o:s:n:h'
+longopts='output-type:,output-path:,output-size:,use-nbd:,help'
 cmdargs=$(emucon_parse_cmdargs -s "${shortopts}" -l "${longopts}" -- "$@")
 if emucon_cmd_failed ; then
 	emucon_abort
@@ -43,8 +54,17 @@ fi
 eval set -- ${cmdargs}
 while true ; do
 	case "$1" in
-		-o|--output-dir)
-			outdir="$2"
+		-t|--output-type)
+			outtype="$2"
+			shift 2 ;;
+		-o|--output-path)
+			outpath="$2"
+			shift 2 ;;
+		-s|--output-size)
+			outsize="$2"
+			shift 2 ;;
+		-n|--use-nbd)
+			nbdpath="$2"
 			shift 2 ;;
 		-h|--help)
 			__print_usage
@@ -59,8 +79,46 @@ while true ; do
 done
 
 # Check arguments
-emucon_check_required_arg "-o/--output-dir" "${outdir}"
-emucon_ensure_dir_exists "${outdir}"
+emucon_check_required_arg "-t/--output-type" "${outtype}"
+emucon_check_required_arg "-o/--output-path" "${outpath}"
+
+# Prepare output image/directory
+case "${outtype}" in
+	tree)
+		emucon_ensure_dir_exists "${outpath}"
+		outdir="${outpath}"
+		;;
+	qcow)
+		emucon_check_required_arg "-s/--output-size" "${outsize}"
+		emucon-qcow create -o "size=${outsize}" "${outpath}" || emucon_abort
+		emucon-qcow mkfs --fs-type ext4 "${outpath}" || emucon_abort
+
+		emucon_print_info "Creating temporary mountpoints for qcow-image..."
+		tmpdir=$(mktemp -d --tmpdir 'emucon-XXXXX')
+		outdir="${tmpdir}/fs"
+		mkdir -v -p "${outdir}"
+		if [ -n "${nbdpath}" ] ; then
+			rawdir="${nbdpath}"
+		else
+			rawdir="${tmpdir}/raw"
+			mkdir -v -p "${rawdir}"
+		fi
+
+		__cleanup() {
+			emucon_print_info 'Cleaning up...'
+			emucon-qcow unmount --fs-path "${outdir}" "${rawdir}" || emucon_abort
+			rm -r -v "${tmpdir}"
+		}
+
+		trap __cleanup EXIT
+
+		emucon-qcow mount --fs-type ext4 --fs-path "${outdir}" "${outpath}" "${rawdir}" || emucon_abort
+		;;
+	*)
+		emucon_print_error "Invalid output type specified: ${outtype}"
+		emucon_abort -v
+		;;
+esac
 
 # Docker names
 tag='eaas/emucon-base-layer:latest'
@@ -70,10 +128,7 @@ emucon_print_info "Building new image for '${tag}'..."
 sudo docker build --no-cache --force-rm=true -t "${tag}" . || emucon_abort -v
 
 emucon_print_info "Creating container from image '${tag}'..."
-container=$(sudo docker create --name "${name}" "${tag}")
-if emucon_cmd_failed ; then
-	emucon_abort -v
-fi
+container=$(sudo docker create --name "${name}" "${tag}") || emucon_abort -v
 
 emucon_print_info "Exporting image to ${outdir}"
 excludes='--exclude=dev/* --exclude=proc/* --exclude=usr/share/doc/* --exclude=usr/share/man/* --exclude=*/__pycache__/*'
--- builder/commands/layer/layers/emulator/run.sh
+++ builder/commands/layer/layers/emulator/run.sh
@@ -9,17 +9,25 @@ __print_usage()
 {
 	cat <<- EOT
 	USAGE:
-	    ${__cmd_name} -b <path> -o <path> [<name>...]
+	    ${__cmd_name} -t <type> -o <path> -b <path> [<name>...]
 
 	DESCRIPTION:
 	    Builds container's emulator layer.
 
 	OPTIONS:
-	    -b, --base-layer <path>
-	        Base layer directory.
+	    -t, --output-type <type>
+	        Output type for built layer:
+	            tree: Filesystem tree
+	            qcow: QCow container image
 
 	    -o, --output-dir <path>
 	        Output directory for built files.
+
+	    -b, --base-layer <path>
+	        Base layer directory or qcow-image.
+
+	    -n, --use-nbd <dev>
+	        Connect qcow-image to specified NBD device.
 
 	    --list
 	        List all supported emulator package names.
@@ -56,22 +64,25 @@ if [ $# -eq 0 ] ; then
 fi
 
 # Parse script's command line arguments
-shortopts='b:o:h'
-longopts='base-layer:,output-dir:,list,help'
-cmdargs=$(emucon_parse_cmdargs -s "${shortopts}" -l "${longopts}" -- "$@")
-if emucon_cmd_failed ; then
-	emucon_abort
-fi
+shortopts='t:o:b:n:h'
+longopts='output-type:,output-dir:,base-layer:,use-nbd:,list,help'
+cmdargs=$(emucon_parse_cmdargs -s "${shortopts}" -l "${longopts}" -- "$@") || emucon_abort
 
 # Lookup parsed parameters and their arguments
 eval set -- ${cmdargs}
 while true ; do
 	case "$1" in
 		-b|--base-layer)
-			basedir="$2"
+			basepath="$2"
+			shift 2 ;;
+		-t|--output-type)
+			outtype="$2"
 			shift 2 ;;
 		-o|--output-dir)
 			outdir="$2"
+			shift 2 ;;
+		-n|--use-nbd)
+			nbdpath="$2"
 			shift 2 ;;
 		--list)
 			__print_emulator_names
@@ -89,10 +100,22 @@ while true ; do
 done
 
 # Check arguments
-emucon_check_required_arg "-b/--base-layer" "${basedir}"
+emucon_check_required_arg "-t/--output-type" "${outtype}"
 emucon_check_required_arg "-o/--output-dir" "${outdir}"
-emucon_ensure_dir_exists "${basedir}"
+emucon_check_required_arg "-b/--base-layer" "${basepath}"
 emucon_ensure_dir_exists "${outdir}"
+case "${outtype}" in
+	tree)
+		emucon_ensure_dir_exists "${basepath}"
+		;;
+	qcow)
+		emucon_ensure_file_exists "${basepath}"
+		;;
+	*)
+		emucon_print_error "Invalid output type specified: ${outtype}"
+		emucon_abort -v
+		;;
+esac
 
 emulators=''
 if [ $# -gt 0 ] ; then
@@ -118,32 +141,51 @@ group=$(id --group --name)
 scripts="$(emucon_get_current_dir)/scripts"
 
 emucon_print_info 'Creating temporary directory...'
-tmpdir=$(mktemp -d '/tmp/emucon-builder-XXXXX')
+tmpdir=$(mktemp -d --tmpdir 'emucon-XXXXX')
 
 # Generate requested layers...
 for emulator in ${emulators} ; do
 	emucon_print_info "Starting emulator-layer generation for ${emulator}..."
 	workdir="${tmpdir}/${emulator}"
-	emuoutdir="${outdir}/${emulator}"
 	mkdir -p "${workdir}"
-	mkdir -p "${emuoutdir}"
+	case "${outtype}" in
+		tree)
+			emuoutpath="${outdir}/${emulator}"
+			mkdir -p "${emuoutpath}"
+			;;
+		qcow)
+			emuoutpath="${outdir}/${emulator}.qcow"
+			emucon-qcow create -o "backing_file=${basepath}" "${emuoutpath}"
+			;;
+	esac
 
 	emucon_print_info "Using working directory: ${workdir}"
 
 	emucon_print_info "Generating config.json for ${emulator}..."
 	emucon-cgen --mount "${scripts}:/emucon-scripts:bind:ro" \
-		-- '/emucon-scripts/install.sh' "${emulator}" \
+		-- /sbin/my_init -- /emucon-scripts/install.sh "${emulator}" \
 		> "${workdir}/config.json"
 
 	emucon_print_info "Running container for ${emulator}..."
-	if emucon-run -w "${workdir}" -l "${basedir}" -u "${emuoutdir}" -c "congen-${emulator}" ; then
-		emucon_print_info "Emulator-layer for ${emulator} created at: ${emuoutdir}"
+	case "${outtype}" in
+		tree)
+			options="-t overlay -l ${basepath} -u ${emuoutpath}" ;;
+		qcow)
+			options="-t qcow -i ${emuoutpath}"
+			if [ -n "${nbdpath}" ] ; then
+				options="${options} --use-nbd ${nbdpath}"
+			fi
+			;;
+	esac
+
+	conid=$(mktemp --dry-run "congen-XXXXX")
+	if emucon-run -w "${workdir}" ${options} -c "${conid}" ; then
+		emucon_print_info "Emulator-layer for ${emulator} created at: ${emuoutpath}"
 	else
 		emucon_print_error "Generating emulator-layer for ${emulator} failed!"
 	fi
 
 	emucon_print_info 'Cleaning up...'
-	rm -v -r ${emuoutdir}/emucon-*
 	rm -v -r "${workdir}"
 done
 
